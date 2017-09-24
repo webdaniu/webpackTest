e6f41e26f50f190f680c03f0fc1e9d7408e1b096
webdaniu <webdaniu@gmail.com>
2017-09-24 20:53:56 +0800

    mm

---
+++ src/chunkThree.js
@@ -0,0 +1,8 @@
+import chunkOne from './chunkOne'
+import chunkTow from './chunkTow'
+
+export default function(){
+console.log('i am san');	
+}
+chunkOne()
+chunkTow()
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import chunkOne from './chunkOne'
 import chunkTow from './chunkTow'
+import chunkThree from './chunkThree.js'
 console.log('i am the entry and inclound chunkOne and chunkTow!');
 chunkOne();
 chunkTow();
+chunkThree();
